12e31f027fa6a31e5019a0bb879f8db4b0667a37
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-13 23:30:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,25 +32,26 @@ class Song
   def self.find_or_create_by_name (name)
      self.find_by_name(name) || self.create_by_name(name)
     end 
-  end 
-  def self.alphabetical(name)
+  
+  def self.alphabetical
     @@all.sort_by{|a| a.name}
   end 
   def self.new_from_filename(name)
     song = self.new 
-    song.name = (name.split("-")[1].chomp("mp3"))
-    song.artist_name = (name.split("-")[0])
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
     song 
   end   
-    def self.new_from_filename(name)
-    song = self.new 
-    song.name = (name.split("-")[1].chomp("mp3"))
-    song.artist_name = (name.split("-")[0])
+    def self.create_from_filename(name)
+    song = self.create 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    binding.pry
+    song.artist_name = (name.split(" - ")[0])
     @@all << self 
     song 
     end
     
-  def self.destroy_all(name)
+  def self.destroy_all
     @@all.clear
   end 
 end
